b253609a744b9f1274b1036c64fca30d580c1991
Виктор <retaow@gmail.com>
2019-02-09 02:26:31 +0300

    New features. PostgreSQL support.

--- src/main/java/com/webapp/GreetingController.java
+++ src/main/java/com/webapp/GreetingController.java
@@ -1,20 +1,44 @@
 package com.webapp;
 
+import com.webapp.domain.Message;
+import com.webapp.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping
-    public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model
-    ) {
-        model.addAttribute("name", name);
+    public String greeting(Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String tag, Model model) {
+        List<Message> messages = messageRepo.findByTag(tag);
+        model.addAttribute("messages", messages);
         return "main";
     }
 
---
+++ src/main/java/com/webapp/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.webapp.repos;
+
+
+import com.webapp.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
